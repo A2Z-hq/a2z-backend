2da1ce609e60364e981c6651fe41a65e672cb58c
Deepak <raj.deeepak@gmail.com>
2018-11-09 23:29:04 +0530

    added dashboard sections

--- admin/index.js
+++ admin/index.js
@@ -12,6 +12,15 @@ router.get('/a2zhq',(req, res, next)=>{
     res.render('index');
 });
 
+router.get('/dashboard',(req, res, next)=>{
+    // res.sendFile('index.html', {root: './admin/public'});
+    // next();
+    res.render('dashboard');
+});
+
+router.post('/login', (req, res)=>{
+    console.log(req);
+});
 
 router.get('/test', (req, res)=>{
     res.send({"message" : "Welcome to Test GOD MODE"})
